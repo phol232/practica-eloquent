b0e5c46b7da093053b48e73fa694af6b6253d1fc
phol <edwinrojastaquiri@gmail.com>
2025-05-21 20:14:41 -0500

    Corregir.

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,18 @@
+<link href="https://unpkg.com/tailwindcss@2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>UNIVERSIDAD CONTINENTAL</h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Cursos</a></li>
+                <li><a href="">Nosotros</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <footer>
+        <p>&copy; 2023 UNIVERSIDAD CONTINENTAL</p>
+    </footer>
+</body>
