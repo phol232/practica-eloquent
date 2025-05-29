1e70010a378f24df9cf3e198d298be8c1531c62c
phol <edwinrojastaquiri@gmail.com>
2025-05-28 19:32:33 -0500

    Cambios

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,18 +27,18 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-            // Nueva ruta admin con middleware web
             Route::middleware('web')
                 ->group(base_path('routes/admin.php'));
-        });
+
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+    });
     }
+
 
     /**
      * Configure the rate limiters for the application.
